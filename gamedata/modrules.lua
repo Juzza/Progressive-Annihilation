baf92a59fbe23a4be541de57b78e211507424c15
Juzza <juzzaman1991@hotmail.com>
2011-10-25 00:05:25 +1100

    Redone, PA

---
+++ gamedata/modrules.lua
@@ -0,0 +1,37 @@
+local modrules  = {
+
+  reclaim = {
+    multiReclaim  = 1;
+    reclaimMethod = 0;
+  },
+repair = {
+	energyCostFactor = 0.25
+
+	},
+
+
+  sensors = {   
+    los = {
+      losMipLevel = 2, 
+      losMul      = 1,
+      airMipLevel = 4,
+      airMul      = 1,
+    },
+  },
+
+  fireAtDead = {
+    fireAtKilled   = false;
+    fireAtCrashing = false;
+  },
+
+  nanospray = {
+    allow_team_colors = true;
+  },
+  
+  featureLOS = { featureVisibility = 2; }
+}
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+return modrules
