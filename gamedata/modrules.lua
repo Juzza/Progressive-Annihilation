175b2b65d549991c340b8c5b4dd94d87f6c8c040
Juzza <juzzaman1991@hotmail.com>
2011-12-15 22:23:23 +1100

    Added Transportability to Ships and Hovers & added some movedefs.

--- gamedata/modrules.lua
+++ gamedata/modrules.lua
@@ -4,11 +4,18 @@ local modrules  = {
     multiReclaim  = 1;
     reclaimMethod = 0;
   },
-repair = {
-	energyCostFactor = 0.25
+ 
+	repair = {
+	energyCostFactor = 0.25,
 
 	},
 
+	Transportability = {
+		TransportAir = 0,
+		TransportShip = 1,
+		TransportHover = 1,
+		TransportGround = 1,
+	},
 
   sensors = {   
     los = {
